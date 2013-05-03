a3fe002f27f53dc165b6bc276ceee9480af87e48
chars32 <chars24@gmail.com>
2013-05-03 16:15:34 -0500

    commit semana 2 desafio 1

---
+++ semana2/Sem2Des1+CarlosGarcia.py
@@ -0,0 +1,17 @@
+nario_entero():
+	#Definimos la funcion con la cual convertiremos los binarios a enteros
+		numero = input("Ingresa el numero binario?: ")												#definimos una variable "numero" la cual almacenara el binaroio                       
+			largo = len(str(numero))			                           								#definimos una variable "largo" para saber el largo de la variable "numero"
+				total = 0											  						   				
+					contador = 1																	
+
+						for i in range(largo):																		#inicamos el ciclo "for" con un rango segun el largo de la variable "largo"
+								constante = 1																			#declaramos la variable "constante" que nos ayudara a sumar uno al valor de contador
+										numero = str(numero)																	#converitmos el valor de numero a string
+												basedos = 2 ** i 																		#declaramos la variable basedos para sacar los exponentes en base dos
+														suma = basedos * int(numero[largo-contador])											#definimos "suma" la cual contendra el valor de la suma + el valor de numero
+																total = total + suma 																	#sumamos todo
+																		contador = contador + constante 														#aumentamos el contador
+																			print "Del numero binario ingresado: %s su numero entero es: %s" % (numero, total)
+
+																			binario_entero()
